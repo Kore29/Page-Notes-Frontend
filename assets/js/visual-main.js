263c570e2e9a712ff42590b5ebd8eae9161f3899
Kore29 <marticastanorodriguez@gmail.com>
2025-01-23 18:03:41 +0100

    Update 1.1.

---
+++ assets/js/visual-main.js
@@ -0,0 +1,16 @@
+const tables = document.querySelectorAll(".main-table"); // Selecciona todas las tablas
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate"); // Añade la clase al entrar
+    } else {
+      entry.target.classList.remove("animate"); // Elimina la clase al salir
+    }
+  });
+});
+
+// Observa cada tabla individualmente
+tables.forEach((table) => {
+  observer.observe(table); // Observa cada tabla individualmente
+});
