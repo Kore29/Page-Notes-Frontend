d51a462ccbb666ffeb7c0175ba55839dae61daae
Kore29 <marticastanorodriguez@gmail.com>
2025-02-05 21:54:26 +0100

    Update 2.2.

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,7 +1,10 @@
 document.getElementById('darkmode-toggle').addEventListener('change', function() {
+    const highlightTheme = document.getElementById('highlight-theme');
     if (this.checked) {
         document.documentElement.setAttribute('data-theme', 'white');
+        highlightTheme.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/atom-one-light.min.css');
     } else {
         document.documentElement.removeAttribute('data-theme');
+        highlightTheme.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/atom-one-dark.min.css');
     }
 });
